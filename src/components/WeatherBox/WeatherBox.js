afdd2ea2b81474ce5076b30661777bc26fd1f801
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-05-20 16:59:21 +0200

    Fix bugs in package.json, complete the app, change node v to 14.x

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,34 +1,53 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
 
   const [weather, setWeather] = useState('');
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
 
   const API_KEY = 'f00b134e81dc7457844bd398abd1431f';
 
   const handleCityChange = useCallback(cityName => {
+
+    setPending(true);
+    setError(false);
+
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main
-        };
-        setWeather(weatherData);
-      });
+    .then(res => {
+      if(res.status === 200) {
+        return res.json()
+          .then(data => {
+      
+            console.log(data);
+
+            const weatherData = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main
+            };
+
+            setWeather(weatherData);
+            setPending(false);
+            
+          })
+        } else {
+          setError(true);
+        }
+     });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary {...weather} />
-      <Loader />
+      { (weather && !pending) && <WeatherSummary {...weather} /> }
+      { (pending && !error) && <Loader /> }
+      { error && <ErrorBox>There is no such city!</ErrorBox> }
     </section>
   )
 };
